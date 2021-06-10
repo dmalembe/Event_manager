d0b7028da917ffcebdd344932b12c633f9e16b07
dmalembe <malembedidier24@gmail.com>
2021-06-10 23:48:51 +0200

    Initialisation du backend

---
+++ eventbackend/src/main/java/com/entreprise/eventbackend/EventbackendApplication.java
@@ -0,0 +1,13 @@
+package com.entreprise.eventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventbackendApplication.class, args);
+	}
+
+}
---
+++ eventbackend/src/test/java/com/entreprise/eventbackend/EventbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.eventbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
