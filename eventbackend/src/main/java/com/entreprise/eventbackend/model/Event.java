cec5c919c8abc389388701a28d70e7f1827588f2
dmalembe <didier-curvier.malembe-ntondele@capgemini.com>
2022-01-26 16:00:53 +0100

    Add Event model

---
+++ eventbackend/src/main/java/com/entreprise/eventbackend/model/Event.java
@@ -0,0 +1,42 @@
+package com.entreprise.eventbackend.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.entreprise.eventbackend.enumeration.Category;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "event")
+@Data
+@NoArgsConstructor
+public class Event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "entitled")
+	private String entitled;
+	
+	@Column(name = "date")
+	private Date date;
+	
+	@Enumerated(EnumType.STRING)
+	private Category category;
+	
+	@Column(name = "adresse")
+	private String Adresse;
+	
+	@Column(name = "capacity")
+	private int capacity;
+}
